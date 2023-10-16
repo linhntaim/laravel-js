8da87ff18e12f5fa77ff670d49d79f823051e615
Nguyen Tuan Linh <linhntaim@users.noreply.github.com>
2023-10-17 03:17:12 +0700

    + framework: rework facade

--- app/providers/route-service-provider.js
+++ app/providers/route-service-provider.js
@@ -1,11 +1,11 @@
+import {App} from '../../lib/framework/support/facades/app.js'
 import {RouteServiceProvider as ServiceProvider} from '../../lib/framework/foundation/support/providers/route-service-provider.js'
-import {Route} from '../../lib/framework/support/facades/route.js'
 
 export class RouteServiceProvider extends ServiceProvider
 {
     boot() {
         this.routes(() => {
-            
+            console.log(App.version())
         })
     }
 }
